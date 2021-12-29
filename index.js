4d31b274d138480bb7e72eb9e1e1c3cd2c69e539
dylandupasquier <dylandupasquier@gmail.com>
2021-12-29 10:09:01 -0800

    FetchAttempt

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function fetchKantoPokemon() {
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+    .then((res) => res.json())
+    .then((pokemonData) => {
+      console.log(pokemonData);
+      pokemonData.results.forEach((pokemon) => {
+        renderAllPokemon(pokemon)
+      })
+    })
+}
 
+
+/// Make a form which allows to users to make a comment on each pokemon
+/// Also a 'like' button. Both of which will update the DOM.
