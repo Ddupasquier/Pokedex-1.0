f07595a3b856537a3326da6f7791982275c389b7
dylandupasquier <dylandupasquier@gmail.com>
2021-12-29 13:03:36 -0800

    functional fetch

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ function fetchKantoPokemon() {
   fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
     .then((res) => res.json())
     .then((pokemonData) => {
-      console.log(pokemonData);
+      console.log(pokemonData.results);
       pokemonData.results.forEach((pokemon) => {
         renderAllPokemon(pokemon)
       })
     })
 }
 
+function renderAllPokemon (pokemon) {
 
+}
+
+fetchKantoPokemon()
 /// Make a form which allows to users to make a comment on each pokemon
 /// Also a 'like' button. Both of which will update the DOM.
