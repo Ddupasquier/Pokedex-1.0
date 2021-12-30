5b5554858251e86821cdb254c78ddb3e4a7e42f9
dylandupasquier <dylandupasquier@gmail.com>
2021-12-30 10:07:07 -0800

    finctioning images rendering in main2

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
+const nav = document.querySelector("#main1");
+const mainCont = document.querySelector('#main2')
+
 function fetchKantoPokemon() {
   fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
     .then((res) => res.json())
     .then((pokemonData) => {
-      console.log(pokemonData.results);
+      // console.log(pokemonData.results);
       pokemonData.results.forEach((pokemon) => {
-        renderAllPokemon(pokemon)
-      })
-    })
+        renderAllPokemon(pokemon);
+      });
+    });
 }
 
-function renderAllPokemon (pokemon) {
+function renderAllPokemon(pokemon) {
+  fetch(pokemon.url)
+    .then((res) => res.json())
+    .then((pokemonData2) => {
+      // console.log(pokemon);
+      const navUL = document.querySelector("#main1ul");
+      const navLI = document.createElement("li");
+      const img = document.createElement("img");
+      img.src =
+        pokemonData2.sprites["versions"]["generation-v"][
+          "black-white"
+        ].animated.front_default;
+      navLI.textContent =
+        pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+      navUL.append(navLI, img);
 
+      // CLICK EVENT
+      img.addEventListener("click", () => renderToMain2(pokemonData2));
+    });
 }
 
-fetchKantoPokemon()
+function renderToMain2(pokemonData2) {
+  mainCont.innerHTML = ""
+  const imgMain = document.createElement('img');
+  imgMain.src = pokemonData2.sprites.other["official-artwork"].front_default;
+  mainCont.append(imgMain);
+}
+
+fetchKantoPokemon();
 /// Make a form which allows to users to make a comment on each pokemon
 /// Also a 'like' button. Both of which will update the DOM.
+/// After multiple refresh order of pokemon is scrambled?
